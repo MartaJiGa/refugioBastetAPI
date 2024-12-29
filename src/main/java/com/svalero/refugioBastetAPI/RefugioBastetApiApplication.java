50c0f730b41d079ae19c9bdb4d6f2011df68ab65
MartaJiGa <103963895+MartaJiGa@users.noreply.github.com>
2024-12-29 19:28:53 +0100

    First commit

---
+++ src/main/java/com/svalero/refugioBastetAPI/RefugioBastetApiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.refugioBastetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugioBastetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugioBastetApiApplication.class, args);
+	}
+
+}
